3be0bf5c1a1975c5f0bc6dbad6f0eeb02bb78c8c
egleticia <leticiaegle@yahoo.com.br>
2023-07-14 19:35:58 -0300

    Inserindo na mesma Solution

---
+++ Exercícios de Aulas/Aula1/Exercicio4/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Globalization;
+
+// Exercício 4: Escreva um programa para calcular a área de um círculo.O valor do raio r será digitado pelo usuário (assumir que serão digitados apenas valores positivos)
+
+Console.WriteLine("Calcular a área de um círculo!");
+Console.WriteLine("Insira o valor do raio do círculo: ");
+
+double raio = double.Parse(Console.ReadLine().Replace(",", "."), CultureInfo.InvariantCulture);
+
+
+double areaCirculo = CalcularRaioCirculo(raio);
+Console.WriteLine($"A área do círculo é {areaCirculo.ToString("F4", CultureInfo.InvariantCulture)}!");
+
+
+
+static double CalcularRaioCirculo(double raio)
+{
+    return Math.PI * Math.Pow(raio, 2);
+}
--- Exercícios de Aulas/Exercicio1/Exercicio1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿
-
-// Exercício 1: Escreva um programa que faça a leitura do nome do usuário e imprima uma saudação no console usando o nome digitado
-
-
-Console.WriteLine("Por favor, insira seu nome: ");
-string nome = Console.ReadLine();
-Console.WriteLine($"Olá! Seja bem vindo(a), {nome}!");
